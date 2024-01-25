bb572f77cf716537301b46a3a052f00dddc1df9f
klchen0112 <klchen0112@gmail.com>
2024-01-25 15:37:10 +0800

    add ltp

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import os
-import jieba
+import torch
+from ltp import LTP
 
 one_gram = {
 
@@ -22,6 +23,10 @@ with open("xkjd/userdict.txt","r") as xk_file:
         xk_dict[word].append(code)
 
         xk_dict[word].sort(key=lambda x:len(x))
+ltp = LTP("LTP/Base2")
+
+if torch.backends.mps.is_available():
+    ltp = ltp.to(torch.device("mps"))
 
 for root, dirs, files in os.walk("data",topdown=False):
     for name in files:
@@ -37,12 +42,20 @@ for root, dirs, files in os.walk("data",topdown=False):
                     word,freq = line.strip().split("\t")
                     freq = int(freq)
                 except:
-                    print(line)
+                    print(line + " error")
                 if word in xk_dict:
                     code = xk_dict[word][0]
                 else:
-                    print(word,"不存在")
-                    continue
+                    code = ""
+                    for word_s in ltp.pipeline(word,tasks="cws").cws:
+                        if word_s in xk_dict:
+                            code += xk_dict[word_s][0]
+                        else:
+                            for char_s in word_s:
+                                if char_s in xk_dict:
+                                    code += xk_dict[char_s][0]
+                                else:
+                                    print(char_s + " not in dict")
 
                 prev_char = None
                 for char in code:
@@ -57,12 +70,12 @@ for root, dirs, files in os.walk("data",topdown=False):
 
 
 with open("results/one_gram.txt","w") as file:
-    for key,value in one_gram.items():
+    for key,value in sorted(one_gram.items(),key = lambda x:x[1]):
         file.write(key+"\t"+str(value)+"\n")
 
 
 with open("results/tow_gram.txt","w") as file:
-    for key,value in tow_gram.items():
+    for key,value in sorted(tow_gram.items(),key = lambda x:x[1]):
         file.write(key+"\t"+str(value)+"\n")
 
 
--- devenv.nix
+++ devenv.nix
@@ -3,8 +3,6 @@
 {
   languages.python = {
     enable = true;
-    version = "3.11.3";
-
     venv.enable = true;
     venv.requirements = ./requirements.txt;
   };
